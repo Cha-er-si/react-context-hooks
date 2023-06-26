4834b9725824c3ae17407017e931278656b37ab6
Cha-er-si <vpcharles31@gmail.com>
2023-06-26 14:24:42 +0800

    useContext hook

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,24 +1,21 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-export class BookList extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isLightTheme, dark, light } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return (
-      <div
-        className="book-list"
-        style={{ color: theme.syntax, background: theme.bg }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>The way of kings</li>
-          <li style={{ background: theme.ui }}>The way of the wind</li>
-          <li style={{ background: theme.ui }}>Disaster Class Hero</li>
-        </ul>
-      </div>
-    );
-  }
-}
+const BookList = () => {
+  const { isLightTheme, dark, light } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div
+      className="book-list"
+      style={{ color: theme.syntax, background: theme.bg }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>The way of kings</li>
+        <li style={{ background: theme.ui }}>The way of the wind</li>
+        <li style={{ background: theme.ui }}>Disaster Class Hero</li>
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
